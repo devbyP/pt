cd09640024a3457101ef971643a90db32bd30dfb
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2023-08-04 07:13:49 +0700

    use bubble tea

--- cli/main.go
+++ cli/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	// "github.com/spf13/cobra"
 )
 
 func main() {
 	fmt.Println("start timer")
-	startTimer()
+	runTryTea()
 }
--- cli/timer.go
+++ cli/timer.go
@@ -2,28 +2,60 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"log"
 	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-var quit = make(chan os.Signal, 1)
+type tryTea struct {
+	current int
+	max     int
+}
+type tickMsg time.Time
 
-func startTimer() {
-	inc := 1
-	t := time.NewTicker(time.Second * time.Duration(inc))
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	count := 0
-	for {
-		select {
-		case <-t.C:
-			count += inc
-			fmt.Printf("\r%d", count)
-		case <-quit:
-			t.Stop()
-			fmt.Println("timer stop")
-			return
+var count int
+
+func (tt tryTea) Init() tea.Cmd {
+	return tick()
+}
+
+func (tt tryTea) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return tt, tea.Quit
+		default:
 		}
+	case tickMsg:
+		if tt.max != 0 && tt.current >= tt.max {
+			return tt, tea.Quit
+		}
+		tt.current++
+		return tt, tick()
 	}
+	return tt, nil
+}
+
+func (tt tryTea) View() string {
+	if tt.current >= tt.max {
+		return "finish counting.\nprogram end.\n\n"
+	}
+	s := fmt.Sprintf("start counting to %d\n\n%d\n", tt.max, tt.current)
+	return s
+}
+
+func runTryTea() {
+	p := tea.NewProgram(tryTea{max: 10})
+	_, err := p.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
